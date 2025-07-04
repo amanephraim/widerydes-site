bf7dcbc6c9eca78c2e7870082b7b1c7b2ec796d1
amanephraim <eph_amanzi@yahoo.co.uk>
2025-07-04 12:14:49 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_B0T8jtG8g', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '4vufc8tjnm31qvl81lvip49cin', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-west-2_ACkU6XPoN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19l3fn9an5pfelhdg51kgg03ad', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-west-2' // e.g. us-east-2
     },
     api: {
